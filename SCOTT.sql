4f485b149da21c0a88f3d06f017979822e87d550
changho <guide@yedam.ac>
2026-02-02 12:34:06 +0900

    for pull

--- SCOTT.sql
+++ SCOTT.sql
@@ -12,4 +12,19 @@ FROM professor;
 SELECT studno, name, grade
 FROM student;
 
--- 숙제완료함.
+-- 숙제완료함.  \'
+select name || '''id is ' || id || ' 이고 ' || grade || '학년입니다.' 
+       as "학년설명" -- 별칭(alias) 
+      --,grade "학년"
+from student;
+-- James Seo의 '아이디'는 75true 이고 4학년입니다. => alias (학년설명)
+
+SELECT distinct name, grade -- 중복된값 제거.
+FROM student;
+
+-- 연습1) 
+
+
+-- 연습2)
+SELECT *
+FROM emp;
